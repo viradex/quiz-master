5cdf40b266c4c13d2fc4072da076063f62dbfc00
Arnav Thorat <arnavthorat78@gmail.com>
2026-04-08 17:59:52 +1000

    Add starting window and about screen

---
+++ app.py
@@ -0,0 +1,94 @@
+# Welcome screen
+import tkinter as tk
+from tkinter import ttk
+
+
+class App:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Quiz Master")
+        self.root.geometry("1000x600")
+        self.root.minsize(800, 600)
+
+        self.frame = ttk.Frame(self.root, padding=20)
+        self.frame.grid(row=0, column=0, sticky="nsew")
+
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+
+        self.frame.columnconfigure(0, weight=1)
+
+        style = ttk.Style()
+        style.configure("BigBtn.TButton", font=("Segoe UI", 16))
+        style.configure("MediumBtn.TButton", font=("Segoe UI", 12))
+        style.configure("SmallBtn.TButton", font=("Segoe UI", 10))
+
+        ttk.Label(
+            self.frame,
+            text="Welcome to Quiz Master!",
+            font=("Segoe UI", 24, "bold"),
+        ).grid(row=0, column=0, sticky="n", pady=(15, 0))
+
+        ttk.Label(
+            self.frame,
+            text="Select game mode:",
+            font=("Segoe UI", 12),
+        ).grid(row=1, column=0, sticky="n", pady=(10, 0))
+
+        ttk.Button(
+            self.frame,
+            text="Start as Server",
+            style="BigBtn.TButton",
+            padding=(50, 10),
+            width=15,
+        ).grid(row=2, column=0, sticky="n", pady=(30, 0))
+        ttk.Button(
+            self.frame,
+            text="Join as Client",
+            style="BigBtn.TButton",
+            padding=(50, 10),
+            width=15,
+        ).grid(row=3, column=0, sticky="n", pady=(10, 0))
+
+        ttk.Button(
+            self.frame,
+            text="Manage Quizzes",
+            style="MediumBtn.TButton",
+            padding=(50, 7),
+            width=18,
+        ).grid(row=4, column=0, sticky="n", pady=(40, 0))
+        ttk.Button(
+            self.frame,
+            text="Settings / Stats",
+            style="MediumBtn.TButton",
+            padding=(50, 7),
+            width=18,
+        ).grid(row=5, column=0, sticky="n", pady=(10, 0))
+
+        self.small_btn_frame = ttk.Frame(self.frame)
+        self.small_btn_frame.grid(row=6, column=0, pady=(40, 0), sticky="n")
+
+        self.small_btn_frame.columnconfigure(0, weight=1)
+        self.small_btn_frame.columnconfigure(1, weight=1)
+
+        ttk.Button(
+            self.small_btn_frame,
+            text="About / Help",
+            style="SmallBtn.TButton",
+            padding=(8, 5),
+            width=15,
+        ).grid(row=0, column=0, padx=(2, 5), sticky="e")
+        ttk.Button(
+            self.small_btn_frame,
+            text="Exit",
+            style="SmallBtn.TButton",
+            padding=(8, 5),
+            width=15,
+        ).grid(row=0, column=1, padx=(2, 0), sticky="w")
+
+    def run(self):
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    App().run()
