b99ddc9ea23d53a769842b71fdc2dd109f61e5d4
Arnav Thorat <arnavthorat78@gmail.com>
2026-04-14 13:37:24 +1000

    Add lobby window for client and add back to menu button for setup window

--- client/setup.py
+++ client/setup.py
@@ -10,7 +10,7 @@ class Setup:
         self.root.geometry("1000x600")
         self.root.minsize(800, 600)
 
-        self.frame = ttk.Frame(self.root, padding=20)
+        self.frame = ttk.Frame(self.root, padding=40)
         self.frame.grid(row=0, column=0, sticky="nsew")
 
         self.root.columnconfigure(0, weight=1)
@@ -24,45 +24,54 @@ class Setup:
 
         style = ttk.Style()
         style.configure("Big.TButton", font=("Segoe UI", 14))
+        style.configure("Medium.TButton", font=("Segoe UI", 12))
         style.configure("Gray.TLabel", foreground="#A7A7A7", font=("Segoe UI", 8))
 
         ttk.Label(
             self.frame,
             text="Enter connection details:",
             font=("Segoe UI", 20),
-        ).grid(row=0, column=0, columnspan=2, sticky="w", pady=(30, 0), padx=(30, 0))
+        ).grid(row=0, column=0, columnspan=2, sticky="w")
 
         ttk.Label(
             self.frame,
             text="Server IP:",
             font=("Segoe UI", 14),
-        ).grid(row=1, column=0, sticky="e", pady=(50, 0), padx=(30, 0))
+        ).grid(row=1, column=0, sticky="e", pady=(50, 0))
 
         ttk.Entry(
             self.frame, textvariable=self.server_ip_var, font=("Segoe UI", 14)
-        ).grid(row=1, column=1, sticky="ew", pady=(50, 0), padx=25)
+        ).grid(row=1, column=1, sticky="ew", pady=(50, 0), padx=(25, 0))
 
         ttk.Label(
-            self.frame, text="Connecting to port: 5555", style="Gray.TLabel"
-        ).grid(row=2, column=1, sticky="e", pady=(5, 0), padx=(0, 25))
+            self.frame, text="Connecting to port: 7878", style="Gray.TLabel"
+        ).grid(row=2, column=1, sticky="e", pady=(5, 0))
 
         ttk.Label(
             self.frame,
             text="Nickname:",
             font=("Segoe UI", 14),
-        ).grid(row=3, column=0, sticky="e", pady=(20, 0), padx=(30, 0))
+        ).grid(row=3, column=0, sticky="e", pady=(20, 0))
 
         ttk.Entry(
             self.frame, textvariable=self.nickname_var, font=("Segoe UI", 14)
-        ).grid(row=3, column=1, sticky="ew", pady=(20, 0), padx=25)
+        ).grid(row=3, column=1, sticky="ew", pady=(20, 0), padx=(25, 0))
+
+        ttk.Button(
+            self.frame,
+            text="Back to Menu",
+            style="Medium.TButton",
+            padding=(30, 5),
+            width=15,
+        ).grid(row=4, column=0, columnspan=2, sticky="w", pady=(40, 0))
 
         ttk.Button(
             self.frame,
             text="Join",
             style="Big.TButton",
-            padding=(70, 5),
-            width=5,
-        ).grid(row=4, column=1, sticky="e", pady=(40, 0), padx=(0, 25))
+            padding=(30, 5),
+            width=15,
+        ).grid(row=4, column=1, sticky="e", pady=(40, 0))
 
     def run(self):
         self.root.mainloop()
