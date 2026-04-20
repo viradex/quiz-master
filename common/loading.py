38d416ba4f16e4eb4518a6f8e608b368b9bc9895
Arnav Thorat <arnavthorat78@gmail.com>
2026-04-20 22:09:10 +1000

    Add animated loading screen

--- common/loading.py
+++ common/loading.py
@@ -18,9 +18,33 @@ class Loading:
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
 
-        ttk.Label(
-            self.frame, text="Loading, please wait...", font=(FONT_FAMILY, 24, "italic")
-        ).pack()
+        self.loading = ttk.Label(
+            self.frame,
+            text="⠋  Loading...",
+            font=(FONT_FAMILY, 32),
+        )
+        self.loading.pack()
+
+        self.action = ttk.Label(
+            self.frame,
+            text="Preparing game...",
+            font=(FONT_FAMILY, 16),
+            foreground=COLOR_DARK_GRAY,
+        )
+        self.action.pack(pady=(10, 0))
+
+        self.spinner = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
+        self.index = 0
+
+        self.animate_loading()
+
+    def animate_loading(self):
+        # Might be an exception for the prototype guidelines ;)
+        frame = self.spinner[self.index]
+        self.loading.config(text=f"{frame}  Loading...")
+
+        self.index = (self.index + 1) % len(self.spinner)
+        self.root.after(80, self.animate_loading)
 
     def run(self):
         self.root.mainloop()
