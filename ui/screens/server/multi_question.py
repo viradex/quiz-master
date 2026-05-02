edfeb2a783fa6ce2f368c5aabd3c511a0f485959
Arnav Thorat <arnavthorat78@gmail.com>
2026-05-02 21:24:38 +1000

    Add custom spinner widget and client lobby screen; update loading screen

--- ui/screens/server/multi_question.py
+++ ui/screens/server/multi_question.py
@@ -2,7 +2,7 @@ from ui.screens.base_screen import BaseScreen
 
 
 class ServerMultiQuestionScreen(BaseScreen):
-    title_text = "Quiz Master – Question 2 / 2"
+    title_text = "Quiz Master – Question 1 / 2"
 
     def __init__(self, parent=None):
         super().__init__(parent)
